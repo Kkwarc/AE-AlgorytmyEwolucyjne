8773b6b7e0eb38509f8ce039cb65e8711e858efb
Kkwarc <kwarc02@gmail.com>
2023-04-07 10:28:44 +0200

    del

--- proj1/test.m
+++
@@ -1,8 +0,0 @@
-opts = optimoptions('ga', 'MaxStallGenerations', 50, 'PopulationSize',50,"MaxGenerations",100);
-
-down_lim = [-5, -5];
-up_lim = [5, 5];
-
-wektor = ga(@(x) fbanan(x, -1, 1.5), 2, [], [], [], [], down_lim, up_lim, [], [], opts);
-disp(wektor)
-disp(fbanan(wektor, -1, 1.5))
